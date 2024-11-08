df05ca67bc24d4d4fbe344f038e1ea61015f040b
onkar69483 <onkar69483@gmail.com>
2024-11-08 14:08:31 +0530

    moved chatbot to components folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,7 +12,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Chatbot from "./pages/Chatbot";
+import Chatbot from "./components/Chatbot";
 
 export default function App() {
   return (
